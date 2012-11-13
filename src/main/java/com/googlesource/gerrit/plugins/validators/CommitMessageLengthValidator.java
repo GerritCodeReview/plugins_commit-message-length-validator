716f5f5b749fa6793aeaf96d15b0cf3972625851
David Pursehouse <david.pursehouse@sonymobile.com>
2012-11-13 18:29:09 +0900

    Initial implementation of commit message length checks
    The initial implementation is based on the checks that were removed
    from core Gerrit in I617cc3c11343d46e2a9c3258b8a24772f4ed72b2
    
    Change-Id: I50ed4a56b2ca7be4c9ced13a404611aa681084e1

---
+++ src/main/java/com/googlesource/gerrit/plugins/validators/CommitMessageLengthValidator.java
@@ -0,0 +1,57 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.validators;
+
+import org.eclipse.jgit.lib.AbbreviatedObjectId;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+import com.google.gerrit.extensions.annotations.Listen;
+import com.google.gerrit.server.events.CommitReceivedEvent;
+import com.google.gerrit.server.git.validators.CommitValidationListener;
+import com.google.gerrit.server.git.validators.CommitValidationResult;
+import com.google.inject.Singleton;
+
+@Listen
+@Singleton
+public class CommitMessageLengthValidator implements CommitValidationListener {
+
+  @Override
+  public CommitValidationResult onCommitReceived(CommitReceivedEvent receiveEvent) {
+    final RevCommit commit = receiveEvent.commit;
+    final AbbreviatedObjectId id = commit.abbreviate(6);
+    CommitValidationResult result = new CommitValidationResult();
+    
+    if (65 < commit.getShortMessage().length()) {
+      result.addMessage("(W) " + id.name() //
+         + ": commit subject >65 characters; use shorter first paragraph");
+    }
+
+    int longLineCnt = 0, nonEmptyCnt = 0;
+    for (String line : commit.getFullMessage().split("\n")) {
+      if (!line.trim().isEmpty()) {
+        nonEmptyCnt++;
+      }
+      if (70 < line.length()) {
+        longLineCnt++;
+      }
+    }
+
+    if (0 < longLineCnt && 33 < longLineCnt * 100 / nonEmptyCnt) {
+      result.addMessage("(W) " + id.name() //
+          + ": commit message lines >70 characters; manually wrap lines");
+    }
+
+    return CommitValidationResult.SUCCESS;
+  }
+}
