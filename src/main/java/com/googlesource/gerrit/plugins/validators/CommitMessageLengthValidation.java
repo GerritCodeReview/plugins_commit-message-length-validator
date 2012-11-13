1257cc6880a18c230f5961706b6d0c4749b1f638
David Pursehouse <david.pursehouse@sonymobile.com>
2012-11-13 18:40:56 +0900

    Make the max commit message subject and line lengths configurable
    Instead of hard-coding the maximum commit message subject and line
    lengths to 65 and 70 characters respectively, make them user
    configurable.
    
    The user can add a new config file "commitmessage.config" in the site's
    etc folder, which specifies the maximum subject and line lengths.
    
    If the config file does not exist, or is empty, the default values (65
    and 70) are used.
    
    Change-Id: I43dacc2dd281fd521a156b19ec145fe299f658ef

--- src/main/java/com/googlesource/gerrit/plugins/validators/CommitMessageLengthValidation.java
+++ src/main/java/com/googlesource/gerrit/plugins/validators/CommitMessageLengthValidation.java
@@ -13,22 +13,70 @@
 // limitations under the License.
 package com.googlesource.gerrit.plugins.validators;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lib.AbbreviatedObjectId;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.util.FS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.gerrit.extensions.annotations.Listen;
+import com.google.gerrit.server.config.SitePaths;
 import com.google.gerrit.server.events.CommitReceivedEvent;
 import com.google.gerrit.server.git.validators.CommitValidationException;
 import com.google.gerrit.server.git.validators.CommitValidationListener;
 import com.google.gerrit.server.git.validators.CommitValidationMessage;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 @Listen
 @Singleton
 public class CommitMessageLengthValidation implements CommitValidationListener {
+  private static Logger log = LoggerFactory.getLogger(CommitValidationListener.class);
+
+  private final static int DEFAULT_MAX_SUBJECT_LENGTH = 65;
+  private final static int DEFAULT_MAX_LINE_LENGTH = 70;
+  private final static String CONFIG_FILE = "commitmessage.config";
+  private final static String COMMIT_MESSAGE_SECTION = "commitmessage";
+  private final static String MAX_SUBJECT_LENGTH_KEY = "maxSubjectLength";
+  private final static String MAX_LINE_LENGTH_KEY = "maxLineLength";
+
+  private int maxSubjectLength;
+  private int maxLineLength;
+
+  @Inject
+  public CommitMessageLengthValidation(final SitePaths site)
+      throws ConfigInvalidException, IOException {
+    final File cfgPath = new File(site.etc_dir, CONFIG_FILE);
+    if (!cfgPath.exists() || cfgPath.length() == 0) {
+      log.warn("Config file " + cfgPath + " does not exist or is empty; using default values");
+    } else {
+      final FileBasedConfig cfg = new FileBasedConfig(cfgPath, FS.DETECTED);
+      try {
+        cfg.load();
+        this.maxSubjectLength = cfg.getInt(COMMIT_MESSAGE_SECTION, MAX_SUBJECT_LENGTH_KEY,
+            DEFAULT_MAX_SUBJECT_LENGTH);
+        this.maxLineLength = cfg.getInt(COMMIT_MESSAGE_SECTION, MAX_LINE_LENGTH_KEY,
+            DEFAULT_MAX_LINE_LENGTH);
+        return;
+      } catch (ConfigInvalidException e) {
+        throw new ConfigInvalidException(String.format(
+            "Config file %s is invalid: %s", cfgPath, e.getMessage()), e);
+      } catch (IOException e) {
+        throw new IOException(String.format(
+            "Cannot read %s: %s", cfgPath,  e.getMessage()), e);
+      }
+    }
+    this.maxSubjectLength = DEFAULT_MAX_SUBJECT_LENGTH;
+    this.maxLineLength = DEFAULT_MAX_LINE_LENGTH;
+  }
+
 
   @Override
   public List<CommitValidationMessage> onCommitReceived(CommitReceivedEvent receiveEvent)
@@ -37,9 +85,10 @@ public class CommitMessageLengthValidation implements CommitValidationListener {
     final AbbreviatedObjectId id = commit.abbreviate(7);
     List<CommitValidationMessage> messages = new ArrayList<CommitValidationMessage>();
 
-    if (65 < commit.getShortMessage().length()) {
+    if (this.maxSubjectLength < commit.getShortMessage().length()) {
       messages.add(new CommitValidationMessage("(W) " + id.name() //
-         + ": commit subject >65 characters; use shorter first paragraph", false));
+         + ": commit subject >" + this.maxSubjectLength //
+         + " characters; use shorter first paragraph", false));
     }
 
     int longLineCnt = 0, nonEmptyCnt = 0;
@@ -47,14 +96,15 @@ public class CommitMessageLengthValidation implements CommitValidationListener {
       if (!line.trim().isEmpty()) {
         nonEmptyCnt++;
       }
-      if (70 < line.length()) {
+      if (this.maxLineLength < line.length()) {
         longLineCnt++;
       }
     }
 
     if (0 < longLineCnt && 33 < longLineCnt * 100 / nonEmptyCnt) {
       messages.add(new CommitValidationMessage("(W) " + id.name() //
-          + ": commit message lines >70 characters; manually wrap lines", false));
+          + ": commit message lines >" + this.maxLineLength //
+          + " characters; manually wrap lines", false));
     }
 
     return messages;
